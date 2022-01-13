c2fa54220c6cca5f86ddd941edcc1c81233b819e
Szyszzz <jakzych@gmail.com>
2022-01-13 09:19:01 +0100

    Added bullet timeout
    - Added bullet timeout. If bullet does not hit anything during it's lifetime, it will get destroyed

--- ChicagoBloodhunt/Assets/Scripts/BulletTravel.cs
+++ ChicagoBloodhunt/Assets/Scripts/BulletTravel.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class BulletTravel : MonoBehaviour
 {
     public float bulletspeed = 10f;
+    public float bulletLifetime = 5f;
     public Rigidbody2D impact;
+
+    private float time;
 
 // Start is called before the first frame update
     void Start()
     {
         impact.velocity = transform.right * bulletspeed;
+    }
+
+    private void Update()
+    {
+        if(bulletLifetime > time)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Bullet time out");
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
